d9961e83a1a221b906837eca30ed819e723a016a
ROMARIO SANTOS <SROMARIO577@GMAIL.COM>
2024-08-04 00:42:27 -0300

    upgrade

--- detection/views.py
+++ detection/views.py
@@ -1,37 +1,40 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from detection.yolo_detector.utils import predict_image, draw_boxes
+from detection.yolo_detector.utils import predict_image, preprocess_image
 from django.views.decorators.csrf import csrf_exempt
 from ultralytics import YOLO
-import os 
+import cv2
+import os
 
+# Carregar o modelo YOLO
 model = YOLO('detection/yolo_detector/weights/best.pt')
 
-#receber post request
+
 @csrf_exempt
 def detect_objects_view(request):
     if request.method == 'POST' and request.FILES.get('image'):
         image_file = request.FILES['image']
         
-        # Salvar a imagem recebida // adaptar ao banco aws
+        # Salvar a imagem recebida
         temp_path = 'temp.jpg'
         with open(temp_path, 'wb+') as destination:
             for chunk in image_file.chunks():
                 destination.write(chunk)
         
-        # inferência yolo
-        isPromoBottle, results_img = predict_image(model, temp_path, conf=0.7, iou=0.5)
+        # Ler a imagem e pré-processar
+        image = cv2.imread(temp_path)
+        if image is None:
+            return JsonResponse({'error': 'Erro ao carregar a imagem'}, status=400)
         
-        # Desenhar caixas caso tenha deteção e remover foto original, salvando apenas nova com deteção
+        prepro_image = preprocess_image(image)
+        
+        # Realizar a inferência
+        isPromoBottle, results_img = predict_image(model, prepro_image, conf=0.75, iou=0.5)
+        
+        # Verificar se houve detecção
         if isPromoBottle:
-            output_path = 'detected_temp.jpg'
-            draw_boxes(temp_path, results_img, model, output_path)
-            os.remove(temp_path)
-            print(f"Imagem original excluída: {temp_path}")
-            
+            return JsonResponse({'detected': True})
+        else:
+            return JsonResponse({'detected': False})
         
-       
-        return JsonResponse({'detected': isPromoBottle})
-
-    
     return JsonResponse({'error': 'Envie uma imagem válida'}, status=400)
--- detection/yolo_detector/utils.py
+++ detection/yolo_detector/utils.py
@@ -1,16 +1,48 @@
 import cv2
 from ultralytics import YOLO
 
-# def para fazer a inferência yolo
-def predict_image(model, image_path, conf=0.7, iou=0.5, imgsz=640):
-    # Ler a imagem 
-    image = cv2.imread(image_path)
+def preprocess_image(image, target_size=(640, 640)):
+    """
+    Pré-processa a imagem para o modelo YOLO.
     
-    # Verificar se a imagem foi carregada corretamente
-    if image is None:
-        print(f"Erro ao carregar a imagem: {image_path}")
-        return False, None  # Retorna False se houver erro ao carregar a imagem
+    Args:
+    - image (numpy.ndarray): Imagem carregada pelo OpenCV.
+    - target_size (tuple): Dimensões para redimensionamento da imagem.
 
+    Returns:
+    - numpy.ndarray: Imagem pré-processada.
+    """
+    # Redimensionar a imagem
+    image_resized = cv2.resize(image, target_size)
+    
+    # Converter para RGB (OpenCV usa BGR por padrão)
+    image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)
+    
+    # Normalizar a imagem
+    image_normalized = image_rgb / 255.0
+
+    # Converter para formato esperado pelo YOLO
+    image_preprocessed = (image_normalized * 255).astype('uint8')
+    
+    return image_preprocessed
+
+
+
+def predict_image(model, image, conf=0.7, iou=0.5, imgsz=640):
+    """
+    Realiza a inferência usando o modelo YOLO.
+
+    Args:
+    - model (YOLO): Modelo YOLO carregado.
+    - image (numpy.ndarray): Imagem pré-processada.
+    - conf (float): Limite de confiança para a detecção.
+    - iou (float): Limite de sobreposição para a detecção.
+    - imgsz (int): Tamanho da imagem de entrada.
+
+    Returns:
+    - bool: Verdadeiro se alguma detecção com confiança > conf for encontrada.
+    - results_img (Results): Resultados da inferência.
+    """
     # Realizar a inferência na imagem
     results_img = model.predict(
         source=image,
@@ -20,33 +52,13 @@ def predict_image(model, image_path, conf=0.7, iou=0.5, imgsz=640):
         stream=False
     )
     
+    
+
     # Verificar se houve detecção
     detections = results_img[0].boxes if results_img else None
     if detections:
         for box in detections:
-            if box.conf > conf:
-                return True, results_img  
+            if box.conf[0] > conf:
+                return True, results_img
     
-    return False, results_img 
-
-# Função para desenhar caixas delimitadoras na imagem e salvar a imagem resultante
-def draw_boxes(image_path, results_img, model, output_path):
-    # Ler a imagem 
-    image = cv2.imread(image_path)
-    
-    if image is None:
-        print(f"Erro ao carregar a imagem: {image_path}")
-        return False
-
-    # Desenhar caixas delimitadoras na imagem
-    detections = results_img[0].boxes if results_img else None
-    if detections:
-        for box in detections:
-            x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
-            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            label = f'{model.names[int(box.cls)]}: {float(box.conf):.2f}'  # Converte box.conf para float
-            cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    
-            # Salvar a imagem com as detecções
-            cv2.imwrite(output_path, image)
-    return True
+    return False, results_img
